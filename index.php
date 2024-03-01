69d09b28718d35585890165495711ecb40de8d9e
Nahuel Stella <nahuelprg@gmail.com>
2024-03-01 17:22:09 -0300

    Imagenes
    Imagenes Relacionadas con el Proyecto Imagen de la base de datos y interfaz de usuario

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                             <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Sistema de Gestion - La Oveja Negra</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Sistema de Gestión - Inicia Sesión</h1>
                                     </div>
                                     
                                     <form class="user" action="index.php" method="POST">
